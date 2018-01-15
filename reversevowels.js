a491fcf927bf43999a996605dd044e1f40ecd27b
Zach Fudge <zacheryfudge@gmail.com>
2018-01-15 01:29:33 -0700

    refactored to es6 syntax

--- reversevowels.js
+++ reversevowels.js
@@ -1,30 +1,30 @@
 function detVowel(word) {
-  var newWord = word.split('');
+  const newWord = word.split('');
  
-  var vowels = wordLoop(word,newWord,function(arr1,arr2,n,arr3) {
+  const vowels = wordLoop(word,newWord,function(arr1,arr2,n,arr3) {
     arr3.push(arr1[n]);
   });  //console.log(vowels);
  
-  var leng = vowels.length-1;  //console.log('vowels: '+ leng);
+  let leng = vowels.length-1;  //console.log('vowels: '+ leng);
  
   wordLoop(newWord,vowels,function(arr,arr2,n,arr3) {  //console.log(arr2[leng]);
     arr[n] = arr2[leng];
     leng--;
   });
  
-  var product = '';
-  for (var i = 0; i < word.length; i++) {
+  let product = '';
+  for (let i = 0; i < word.length; i++) {
     product += newWord[i];
   }
   console.log(product);
 }
 
+
+// Higher-order function, iterates over the array
 function wordLoop(array,array2,fn) {
-  var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === 'a' || array[i] === 'e' || array[i] === 'i' || array[i] === 'o' || array[i] === 'u') {
-      fn(array,array2,i,newArray);
-    }
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'a' || array[i] === 'e' || array[i] === 'i' || array[i] === 'o' || array[i] === 'u') fn(array,array2,i,newArray);
   }
   return newArray;
 }
